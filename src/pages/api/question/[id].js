3506f0efc6a088e2e7bb0654ecb3ae88e03629b9
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-07-20 02:12:54 -0300

    ✨feat: create api and componente question

---
+++ src/pages/api/question/[id].js
@@ -0,0 +1,13 @@
+const question = (req, res) => {
+    if(req.method !== 'GET') res.status(405).send();
+    const { id } = req.query;
+    res.status(200).json({
+        id, 
+        question: 'What your favority color?',
+        responses: [
+            'white', 'black', 'blue', 'gray'
+        ]
+    })
+}
+
+export default question;
---
+++ src/pages/rotas/[id]/question.jsx
@@ -0,0 +1,39 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+
+const Question = () => {
+    const router = useRouter()
+    const { id } = router.query
+    const [question, setQuestion] = useState(null)
+
+    useEffect(() => {
+        console.log(id);
+        fetch(`http://localhost:3000/api/question/${id}`)
+        .then(response => response.json())
+        .then(setQuestion)
+    }, [])
+
+    const renderResponses = () => {
+        if(!question) return false;
+        return question.responses.map((item, i) => {
+            return <li key={i}>{item}</li>
+        })
+    }
+    
+    return (
+        <>
+            <h1>
+                {question?.id} - Question
+            </h1>
+            <div>
+                <span>{question?.question}</span>
+                <ul>
+                    {renderResponses()}
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Question;
